33818440883825d5b4462c279c91710b2da2aa0c
Niklas <niklas.roll123@gmail.com>
2019-09-04 13:14:04 +0200

    config comment

--- Firmware/Config.h
+++ Firmware/Config.h
@@ -2,14 +2,14 @@
 #define Config_h
   //chassis
   //chain right
-  const int ENA = 9;
-  const int in1 = 8;
-  const int in2 = 10;
+  const int ENA = 9;    //digitalPin + PWM
+  const int in1 = 8;    //digitalPin
+  const int in2 = 10;   //digitalPin
   
   //chain left
-  const int ENB = 11;
-  const int in3 = 13;
-  const int in4 = 12;
+  const int ENB = 11;   //digitalPin  + PWM
+  const int in3 = 13;   //digitalPin
+  const int in4 = 12;   //digitalPin
 
   //Serial
   const int baudRate = 1000000;
