966cc5b63d1cbfe0a291c88549db4832d17d19ae
Niklas Roll <niklas.roll123@gmail.com>
2020-06-05 17:57:51 +0200

    New pcb Update

--- Firmware/Config.h
+++ Firmware/Config.h
@@ -3,12 +3,12 @@
   //chassis
   //drv8833
   //chain right  
-  const int in1 = 12; 
-  const int in2 = 13;   
+  const int in1 = 14; 
+  const int in2 = 15;   
   
   //chain left
-  const int in3 = 15;    
-  const int in4 = 14;  
+  const int in3 = 12;    
+  const int in4 = 13;  
   
   //Serial
   const unsigned long baudRate = 1000000;
